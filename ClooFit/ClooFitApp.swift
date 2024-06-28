5e3539cdf51ede128316aee86412f9782e804ebc
Claudio Sottile <claudiosottile02@gmail.com>
2024-06-28 12:45:46 +0200

    Initial Commit

---
+++ ClooFit/ClooFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClooFitApp.swift
+//  ClooFit
+//
+//  Created by Claudio Sottile on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ClooFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
