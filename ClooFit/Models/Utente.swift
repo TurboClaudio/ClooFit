16739dace266572a1923c51400b4060dbdffbd9b
Salvuccio03 <salvatoreoteri03@gmail.com>
2024-07-05 15:18:51 +0200

    Utente

---
+++ ClooFit/Models/Utente.swift
@@ -0,0 +1,15 @@
+//
+//  Utente.swift
+//  ClooFit
+//
+//  Created by Oteri Salvatore on 05/07/24.
+//
+
+import Foundation
+
+
+struct Utente{
+    var eta: Int
+    var nome: String
+    var preferenzeStile: String
+}
