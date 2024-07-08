4241b150f90399933e7758564013cbb27dca1f6f
poolsrt <poolsortino@gmail.com>
2024-07-08 17:35:16 +0200

    aggiunte view del closet

---
+++ ClooFit/Views/ClosetView/ItemViewDetail.swift
@@ -0,0 +1,18 @@
+//
+//  ItemViewDetail.swift
+//  ClooFit
+//
+//  Created by Paolo Sortino on 08/07/24.
+//
+
+import SwiftUI
+
+struct ItemViewDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemViewDetail()
+}
