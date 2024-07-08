8e715f8e062589f8c6f94a48966b47d88db5e464
poolsrt <poolsortino@gmail.com>
2024-07-08 17:36:04 +0200

    aggiunte view del closet

--- ClooFit/Views/ClosetView/ItemViewDetail.swift
+++ ClooFit/Views/ClosetView/ItemViewDetail.swift
@@ -7,12 +7,63 @@
 
 import SwiftUI
 
+// 167, 115
+// 220, 168
+
 struct ItemViewDetail: View {
+    var item: article
+        
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            
+            item.image
+            
+            Text(item.mainColor.capitalized + " " + item.name.capitalized)
+                .font(.largeTitle)
+                .bold()
+            HStack {
+                Image(systemName: "tshirt")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .padding()
+                Text("\(item.material)")
+                    .font(.system(size: 25))
+            }
+            .padding(.horizontal)
+            
+            HStack {
+                Image(systemName: "graduationcap")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .padding()
+                Text("\(item.elegance)")
+                    .font(.system(size: 25))
+            }
+            .padding(.horizontal)
+            
+            HStack {
+                Image(systemName: "cloud.sun")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .padding()
+                Text("\(item.weather)")
+                    .font(.system(size: 25))
+            }
+            .padding(.horizontal)
+            
+            // Text("Material: \(item.material)")
+            // Text("Color: \(item.mainColor)")
+            // Text("Elegance: \(item.elegance)")
+            // Text("Weather: \(item.weather)")
+            Spacer()
+        }
+        .padding()
+        // .navigationTitle(item.name)
     }
 }
 
+
+
 #Preview {
-    ItemViewDetail()
+    ItemViewDetail(item: catalogo[2])
 }
