a8ff5679aea2d1e3181338c21c77c11d378b1f71
Salvuccio03 <salvatoreoteri03@gmail.com>
2024-07-09 09:47:58 +0200

    fix

--- ClooFit/Utils/generateOutfit.swift
+++ ClooFit/Utils/generateOutfit.swift
@@ -11,11 +11,11 @@ func generateOutfit(array:[article], weather: String, gender: String, elegance: 
     var upper: article
     var lower: article
     var subArray: [article] = weatherFilter(from: genderFilter(from: eleganceFilter(from: typeFilter(from: array, type: "top") ?? array , elegance: elegance) ?? array , gender: gender) ?? array , weather: weather) ?? array
-    upper = subArray.randomElement() ?? catalogo[0]
+    upper = subArray.randomElement() ?? catalogo[2]
     subArray = weatherFilter(from: genderFilter(from: eleganceFilter(from: typeFilter(from: array, type: "bottom") ?? catalogo, elegance: elegance) ?? catalogo, gender: gender) ?? catalogo, weather: weather) ?? catalogo
     var count = 0
     repeat{
-        lower = subArray.randomElement() ?? catalogo[1]
+        lower = subArray.randomElement() ?? catalogo[32]
         count += 1
     }while(areColorsHarmonious(upper: upper, lower: lower) && count <= 10)
     
@@ -24,3 +24,7 @@ func generateOutfit(array:[article], weather: String, gender: String, elegance: 
     
     
 }
+
+/*notDefaultFilter(array: [Outfit]) -> [Outfit]{
+    
+}*/
