d3b0d593d5bbb54c54aa78ddc146786c61786e66
poolsrt <poolsortino@gmail.com>
2024-07-10 20:04:25 +0200

    aggiunta vista meteo dinamica dipendente dalla posizione

---
+++ ClooFit/DataHandling/CurrentPosition.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentPosition.swift
+//  ClooFit
+//
+//  Created by Paolo Sortino on 10/07/24.
+//
+
+import Foundation
---
+++ ClooFit/Views/MeteoView.swift
@@ -0,0 +1,18 @@
+//
+//  MeteoView.swift
+//  ClooFit
+//
+//  Created by Paolo Sortino on 10/07/24.
+//
+
+import SwiftUI
+
+struct MeteoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeteoView()
+}
