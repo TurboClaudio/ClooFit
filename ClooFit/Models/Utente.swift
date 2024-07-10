12321c4281885c10a476e22b286ad93fe04a58b4
Salvuccio03 <salvatoreoteri03@gmail.com>
2024-07-10 09:38:48 +0200

    novità generali

--- ClooFit/Models/Utente.swift
+++ ClooFit/Models/Utente.swift
@@ -9,8 +9,16 @@ import Foundation
 
 
 struct Utente: Codable{
-    var eta: Int
-    var nome: String
-    var preferenzeStile: String
-    var stagione: Int
+    var age: Int
+    var gender: String
+    var eyeColor: String
+    var eyeColorRGB: colorRGB
+    var hairColor: String
+    var hairColorRGB: colorRGB
+    var skinColor: String
+    var skinColorRGB: colorRGB
+    /*var stagione: Int{
+        return getStagione()
+    }*/
+    var isTest: Bool
 }
