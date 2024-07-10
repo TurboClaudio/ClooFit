e37ebc7574640729a20d6ea7e6e3abcfb0f96dbf
Salvuccio03 <salvatoreoteri03@gmail.com>
2024-07-10 12:56:21 +0200

    x

--- ClooFit/Models/Utente.swift
+++ ClooFit/Models/Utente.swift
@@ -12,11 +12,8 @@ struct Utente: Codable{
     var age: String
     var gender: String
     var eyeColor: String?
-    var eyeColorRGB: colorRGB?
     var hairColor: String?
-    var hairColorRGB: colorRGB?
     var skinColor: String?
-    var skinColorRGB: colorRGB?
     var stagione: Int?
     var isTest: Bool
 }
